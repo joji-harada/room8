042a64beb1df575a725e0610f08d2034abee9c54
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-10-30 16:11:34 -0700

    Added alert list items. Some styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Welcome from './components/welcome'
+import Alerts from './components/alerts'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Welcome name='bob'/>
+      <div className='container centered'>
+        <Welcome name='bob'/>
+        <Alerts messages={['clean the fucking dishes', 'mow the damn lawn', 'pay your utility bill']}/>
+      </div>
     </div>
   );
 }
